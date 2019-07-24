d95d39a3ea0b891d33a442d6d3e8368a247dc6d9
angelsophia1 <angelsunyulei@126.com>
2019-07-24 17:31:43 +0800

    Initial commit

---
+++ Assets/Scripts/BillBoard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S3
+{
+    public class BillBoard : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.LookAt(Camera.main.transform);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S3
+{
+    public class Bullet : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            GameObject hit = collision.gameObject;
+            Health health = hit.GetComponent<Health>();
+
+            if (health!=null )
+            {
+                health.TakeDamage(10);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
+
